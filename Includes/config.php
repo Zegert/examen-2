02a660368cd06e68cb59430b818141a59755d23a
Zegert Boele <83504@glr.nl>
2021-05-19 19:36:05 +0200

    changes

--- Includes/config.php
+++ Includes/config.php
@@ -4,9 +4,9 @@ session_regenerate_id();
 error_reporting(E_ALL);
 ini_set('display_errors', '0');
 
-$dsn = "mysql:host=?;dbname=?";
-$DB_username = "?";
-$DB_password = "?";
+$dsn = "mysql:host=localhost;dbname=examen";
+$DB_username = "zegert";
+$DB_password = "3312lr2001";
 
 try {
     $conn = new PDO($dsn, $DB_username, $DB_password);
